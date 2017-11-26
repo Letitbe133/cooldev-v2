28a80d6589af2b62457ba6a7e635cfaa9f9defd4
mi <mi@linux.home>
2017-11-26 10:28:10 +0100

    bio Mi

--- private/view/cards-testimonial.php
+++ private/view/cards-testimonial.php
@@ -10,15 +10,15 @@
 	      <a href="https://twitter.com/cooldev_crew" target="_blank"><img src="assets/img/twitter.png" alt="Lien vers profil Twitter"></a>
 	    </h2></div>
 	      
-					<h3>Webdesigner</h3>
-					<p>CEO/CXO &amp Co-founder</p>
+					<h3>Webdesigner / Front-end</h3>
+					<p style="font-size: 1.5rem;">CEO/CXO &amp Co-founder</p>
 	     
 	    </div>
 	    <div class="card-reveal ">
 	      <span class="card-title grey-text text-darken-4">Principales caractéristiques<i class="material-icons right">close</i></span>
-	      <p>La plus créative et la moins têtue des deux. Seriale entrepreneuse, elle connait bien les problématiques des <strong>porteurs de projets </strong>! Son alimentation à base de cookies (recette disponible sur simple demande) et de jus de carottes est le secret de son empathie <3 </p>
-	    </div>
+	      <p>Le web est mon terrain de jeu et j'ai fait d'une de mes passions mon métier*. Quand j'ai créé mon premier site web, je me suis sentie comme "Harry Potter". Mettre ma créativité au service des idées et traduire tout cela en code, je trouve cela magique !<br><img src="assets/img/picto-magic.png" alt=""> <br/><br/> * Mes autres passions secrètes : <a href=""> les cookies</a> et <a href="">le tricot</a></p>
 	  </div>
+	</div>
 	</div>
 	<div class="col s10 offset-s1 m5 l4 offset-l2">
 	  <div class="card ">
@@ -30,11 +30,11 @@
 	      <span class="card-title activator"><h2 >Lionel <a href="https://www.facebook.com/lionel.lafont.52" target="_blank"><img src="assets/img/facebook.png" alt="Lien vers profil Facebook"></a>
 	      <a href="https://twitter.com/LafontLft" target="_blank"><img src="assets/img/twitter.png" alt="Lien vers profil Twitter"></a></h2></span>
 					<h3>Développeur web</h3>
-					<p>CTO &amp Co-founder</p>
+					<p style="font-size: 1.5rem;">CTO &amp Co-founder</p>
 		</div>
 	    <div class="card-reveal">
 	      <span class="card-title grey-text text-darken-4">Principales caractéristiques<i class="material-icons right">close</i></span>
-				<p>Le code c'est mon truc et je peux y passer des heures. Si vous me cherchez je suis probablement dans les coulisses de votre <strong>site web</strong>. Et si je ne suis pas vissé devant mon écran je suis probablement en trek avec mon chien Spud parce que respirer l'air frais c'est bien aussi :) </p>
+				<p>Le code c'est mon truc et je peux y passer des heures. Si vous me cherchez je suis probablement dans les coulisses de votre <strong>site web</strong>. Et si je ne suis pas vissé devant mon écran je suis probablement en trek avec mon chien Spud parce que respirer l'air frais c'est bien aussi :) <br><img src="assets/img/picto-dog.png" alt=""></p>
 	    </div>
 	  </div>
 	</div>
